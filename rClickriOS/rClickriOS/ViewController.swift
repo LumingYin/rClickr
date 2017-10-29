c7b1b7dfdf5f4f6bd2452f4a2f110c831ecf3253
Numeric <>
2017-10-28 20:28:55 -0400

    stub out uii

--- rClickriOS/rClickriOS/ViewController.swift
+++ rClickriOS/rClickriOS/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseCommunity
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func prevSlide(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextSlide(_ sender: UIButton) {
+    }
 
+    
 }
 
